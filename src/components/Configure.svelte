0036ac9947f64a89c74419efc9d185496bce4246
Adam Staveley <adam.staveley@energyweb.org>
2020-07-17 17:09:04 +0200

    configuration form

--- src/components/Configure.svelte
+++ src/components/Configure.svelte
@@ -1,22 +1,55 @@
 <script>
     import { options, stages } from '../stores/options'
+    let spender = false;
 </script>
 
 <style>
+    form {
+        margin: 1em 0;
+    }
     .stage-input {
         cursor: pointer;
-        
+    }
+    .stage-input.selected {
+        color: var(--accent-dark);
+    }
+    .signer-input {
+        width: 100%;
+        background-color: var(--dark);
+        border: none;
+        border-bottom: 1px solid var(--accent-dark);
+        color: var(--accent-dark);
+        font-family: var(--monospace);
+        /* line-height: 1.5; */
+        font-size: 1em;
     }
 </style>
 
 <div class="block">
-    <h4>environment</h4>
+    
+    <form>
+        environment:
+        {#each stages as stage}
+            <label class="stage-input {$options.stage === stage ? 'selected' : ''}">
+                <input type="radio" bind:group={$options.stage} value={stage}/>
+                {stage}
+            </label>
+        {/each}
+    </form>
 
-    {#each stages as stage}
-        <label class="stage-input">
-            <input type="radio" bind:group={$options.stage} value={stage}/>
-            {stage}
+    <form>
+        signer
+        <input class="signer-input" type="password" bind:value={$options.signer} placeholder="0x..."/>
+    </form>
+
+    <form>
+        <label>
+            spender?
+            <input type="checkbox" bind:checked={spender}/>
         </label>
-    {/each}
+        {#if spender || $options.spender}
+            <input class="signer-input" type="password" bind:value={$options.spender} placeholder="0x..."/>
+        {/if}
+    </form>
 
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,38 +2,8 @@
 	import Configure from "../components/Configure.svelte";
 	import { options } from "../stores/options"
 </script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
 
 <svelte:head>
@@ -43,14 +13,9 @@
 <Configure/>
 
 <div class="block">
-	<h1>Great success!</h1>
-	
-	<figure>
-		<img alt='Success Kid' src='successkid.jpg'>
-		<figcaption>Have fun with Sapper!</figcaption>
-	</figure>
-	
-	<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+	<h2>Interact with the registry</h2>
 
 	<p>You are using {$options.stage} as your environment.</p>
+	<p>You sign your data with the private key {$options.signer}</p>
+	<p>You pay for transactions with private key {$options.spender}</p>
 </div>
