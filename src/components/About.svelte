ef85f7c9af89684cc38ab3787086431565bd6fc9
simon <sjtucker28@gmail.com>
2020-08-25 19:50:18 +0100

    ContractData tabs

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,17 +1,21 @@
-<div class="block">	
+<div class="block">
 	<p>
-		This is a client for the 
+		This is a client for the
 		<a href="https://shareandcharge.atlassian.net/wiki/spaces/OCN/overview">
-			Open Charging Network</a>'s decentralized address book of participants, 
-			also known as the
-		<a href="https://bitbucket.org/shareandcharge/ocn-registry">OCN Registry</a>.
+			Open Charging Network
+		</a>
+		's decentralized address book of participants, also known as the
+		<a href="https://bitbucket.org/shareandcharge/ocn-registry">OCN Registry</a>
+		.
 	</p>
 
-	<p>There is no backend. You communicate with the OCN Registry smart contracts
+	<p>
+		There is no backend. You communicate with the OCN Registry smart contracts
 		directly.
 	</p>
 
-	<p>Right now you can only view data. Further updates will add the ability to 
+	<p>
+		Right now you can only view data. Further updates will add the ability to
 		modify state in the smart contracts.
 	</p>
 
@@ -20,7 +24,11 @@
 		future.
 	</p> -->
 
-	<p>Don't trust this deployment? If you prefer, you can 
-		<a href="https://github.com/adamstaveley/ocn-registry-ui">check out the source</a> 
-		and follow the instructions to run it yourself.</p>
+	<p>
+		Don't trust this deployment? If you prefer, you can
+		<a href="https://github.com/adamstaveley/ocn-registry-ui">
+			check out the source
+		</a>
+		and follow the instructions to run it yourself.
+	</p>
 </div>
